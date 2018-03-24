a1e267dab88a561aaa2b1a5f2af24ee00b321060
Guy Pender <guy.pender@me.com>
2018-03-24 21:05:00 +0000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('newLocationMessage',function(message){
 // );
 jQuery('#message-form').on('submit',(e)=>{
     e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage',{
         from:'fred',
-        text:jQuery('[name=message]').val()
+        text:messageTextBox.val()
         },
         function(message){
+            messageTextBox.val('');
             console.log(message);
         }
     );
@@ -46,7 +48,10 @@ locationButton.on('click',()=>{
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled','disabled').text('Sending location .......');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         console.log(position);
         socket.emit('createLocationMessage',{
             latitude:position.coords.latitude,
@@ -54,6 +59,7 @@ locationButton.on('click',()=>{
         });
     },function(){
         alert('Unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send location');
     });
 })
 //www.googlemaps.com/maps?q=50.82296536934156,-0.47751036769027094
